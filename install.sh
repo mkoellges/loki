339e491d19aabbb8eae912214ce3898e0b397fbc
Manfred Köllges <manfred.koellges@me.com>
2022-05-16 15:31:10 +0200

    changes

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 kubectl create ns loki
+
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
+
 kubens loki
 
 helm upgrade --install loki grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,prometheus-node-exporter.hostRootFsMount.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=hostpath,loki.persistence.size=5Gi
 
-kubectl port-forward --namespace loki service/loki-grafana 3000:80
+kubectl create ingress grafana --class=nginx \
+  --rule="grafana.example.com/*=loki-grafanao:80"
